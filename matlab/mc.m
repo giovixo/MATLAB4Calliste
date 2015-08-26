025ba59e0aae6ab547338c35ec1fbfffb414129a
Giovanni De Cesare <decesare.giovanni@gmail.com>
2015-08-26 16:49:32 +0200

    mc.m displays the elapsed time

--- matlab/mc.m
+++ matlab/mc.m
@@ -2,9 +2,9 @@
 % Version 0.1
 %% Step 1: read the file and return the data array
 disp('> Step 1...');
-A = mc_read('PolCrabCusp_short.dat');
+ticID = tic; A = mc_read('PolCrabCusp_short.dat'); toc(ticID);
 disp('> The row photons history A is ready')
 %% Step 2: Convert A into a 'photons history' into a pixelated detector
 disp('> Step 2...');
-Apix = mc_pixelize(A);
+ticID = tic; Apix = mc_pixelize(A); toc(ticID);
 disp('> The pixelated photons history Apix is ready');
