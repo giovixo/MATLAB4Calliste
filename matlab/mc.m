4a4fe0234f2ffba3e901d97fa8f2ced2d38596ae
Giovanni De Cesare <decesare.giovanni@gmail.com>
2015-08-27 15:18:48 +0200

    Step 1 analysis example is include in the main script

--- matlab/mc.m
+++ matlab/mc.m
@@ -4,6 +4,8 @@
 disp('> Step 1...');
 ticID = tic; A = mc_read('PolCrabCusp_short.dat'); toc(ticID);
 disp('> The row photons history A is ready')
+%% Step 1 analysis example (see 'help mc_tools' for more)
+log_text1 = mc_tools1(A, 2);
 %% Step 2: Convert A into a 'photons history' into a pixelated detector
 disp('> Step 2...');
 ticID = tic; Apix = mc_pixelize(A); toc(ticID);
