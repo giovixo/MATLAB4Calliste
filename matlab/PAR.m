d8d4745f0f40b6b6fdd623212444a2278562d20d
Giovanni De Cesare <decesare.giovanni@gmail.com>
2015-08-27 13:55:37 +0200

    PAR.m introduces some parameters that can be used to set the MATLAB functions.

---
+++ matlab/PAR.m
@@ -0,0 +1,11 @@
+classdef PAR
+    %PAR Some constant used for the data analysis 
+    %   Define some parameters, tha can be used into the MATLAB fiunctions 
+    
+    properties( Constant )
+        EVENTS_NUMBER = 500; % The numeber of photons 
+        HISTORY_LENGTH = 20; % The recorded history for the pixelated detector
+    end
+    
+end
+
