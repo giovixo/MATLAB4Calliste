1d2027c631feb0a981b4cd6fe82977115d2f5eb5
Giovanni De Cesare <decesare.giovanni@gmail.com>
2015-08-28 14:02:13 +0200

    Bug fixed: binning problem

--- matlab/mc_tools1.m
+++ matlab/mc_tools1.m
@@ -8,41 +8,41 @@ function [ log_text ] = mc_tools1(A, n)
 %   log_text = MC_TOOLS1(A, n)
 %   Input:
 %   A is array in workspace that contains the row photons history
-%   n is an integer to set the output you wish
-%   n = 1 -> Input spectrum
-%   n = 2 -> Detected spectrum
+%   n is an integer used to set the output you wish
+%   n = 1 -> Plot the input spectrum
+%   n = 2 -> Plot the detected spectrum
 
+    size = PAR.EVENTS_NUMBER;
+ 
+    % Read the input energy
+    size = PAR.EVENTS_NUMBER; 
+    e_inp = zeros(size,1);
+    for i = 1:size
+        e_inp(i) = -1000. * A(A(:,2,i) < 0, 2, i);
+    end
+    [spec, xout]=hist(e_inp,100);
+    
+    % Read the detected energy
+    size = PAR.EVENTS_NUMBER; 
+    e_det = zeros(size,1);
+    for i = 1:size
+        e_det(i) = 1000. * sum( A(A(:,2,i) > 0,2,i) );
+    end
+    
     switch n
         case 1
-            % Read the input energy
-            size = PAR.EVENTS_NUMBER; 
-            e_inp = zeros(size,1);
-            for i = 1:size
-                e_inp(i) = -1000. * A(A(:,2,i) < 0, 2, i);
-            end
-            % and plot the spectrum
-            [spec, xout]=hist(e_inp,10);
-            loglog(xout, spec);
+            loglog(xout, spec,'b');
             grid on;
-            title('Input spectrum');
             xlabel('Energy (keV)','fontsize',14);
             ylabel('Photons','fontsize',14);
-            log_text = '> The input spectrum is ready.';
+            log_text = '> The input spectrum (blue line) is ready.';
         case 2
-            % Read the detected energy
-            size = PAR.EVENTS_NUMBER; 
-            e_det = zeros(size,1);
-            for i = 1:size
-                e_det(i) = 1000. * sum( A(A(:,2,i) > 0,2,i) );
-            end
-            % and plot the spectrum
-            [spec, xout]=hist(e_det,100);
+            spec=histc(e_det,xout);
             loglog(xout, spec,'r');
             grid on;
-            title('Detected spectrum');
             xlabel('Energy (keV)','fontsize',14);
             ylabel('Photons','fontsize',14);
-            log_text = '> The detected spectrum is ready.';
+            log_text = '> The detected spectrum (red line) is ready.';
         otherwise
             log_text = '> Error: the index is not valid. Try <help mc_tools1> for help';
     end
--- matlab/mc_tools2.m
+++ matlab/mc_tools2.m
@@ -1,9 +1,14 @@
 function [ log_text ] = mc_tools2( Apix )
-%MC_TOOLS2 Level2 data analysisA
+%MC_TOOLS2 Level2 data analysis
 %Version 0.1
 %
 %   This functions takes as input the photon list Apix (run mc_pixelize.m first) 
-%   in the pixelated detector and process the data prviding some outputs.
+%   in the pixelated detector and process the data providing some outputs.
+%
+%   log_text = MC_TOOLS2(Apix, n)
+%   Apix is array in workspace that contains the photons history in the
+%   pixelated detector.
+%   n is an integer used to set the output you wish
 
 log_text = 'Processing done.';
 end
