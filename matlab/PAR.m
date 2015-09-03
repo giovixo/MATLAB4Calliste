1bc4c42988211cd7f5acf5cbf3bc9f239d3f99c4
Giovanni De Cesare <decesare.giovanni@gmail.com>
2015-09-03 16:39:09 +0200

    Added function mc_polarization to estimate the polarization factor Q versus angle

--- matlab/PAR.m
+++ matlab/PAR.m
@@ -6,6 +6,7 @@ classdef PAR
         DATA_PATH = '/Users/giovannidecesare/Works/Cusp/MATLAB4Cusp/dataset/set02/';
         EVENTS_NUMBER = 500000; % The numeber of photons 
         HISTORY_LENGTH = 20; % The recorded history for the pixelated detector
+        POLARIZATION_ANGLES = [0, 10, 20, 30, 40, 45, 50, 60, 70, 80 90];
     end
     
 end
---
+++ matlab/mc_polarization.m
@@ -0,0 +1,8 @@
+function [ output_args ] = mc_polarization( input_args )
+%MC_POL Evaluate the polarization at many angles
+%
+%   The angles values are inside the PAR.POLARIZATION_ANGLES array
+
+
+end
+
