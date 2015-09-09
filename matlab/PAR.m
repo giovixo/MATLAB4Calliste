8848a9674a3e27c591e2f72a26faed3ee404d4c0
Giovanni De Cesare <decesare.giovanni@gmail.com>
2015-09-09 11:47:13 +0200

    Possibility to switch off the log. Parameter PAR.POLARIZATION_ANGLES is setted for a nice Q vs theta plot

--- matlab/PAR.m
+++ matlab/PAR.m
@@ -3,10 +3,11 @@ classdef PAR
     %   Define some parameters, tha can be used into the MATLAB fiunctions 
     
     properties( Constant )
+        LOG = 0; % Log level 
         DATA_PATH = '/Users/giovannidecesare/Works/Cusp/MATLAB4Cusp/dataset/set02/';
         EVENTS_NUMBER = 500000; % The numeber of photons 
         HISTORY_LENGTH = 20; % The recorded history for the pixelated detector
-        POLARIZATION_ANGLES = [10, 22.5, 30, 40, -45, 50, 60, 70, 80, 100, 110, 120, 130, 140, 150, 160, 170];
+        POLARIZATION_ANGLES = [5:10:85 95:10:175 185:10:265 275:10:355]; 
     end
     
 end
--- matlab/mc_polarization.m
+++ matlab/mc_polarization.m
@@ -1,9 +1,9 @@
 function [angle, Q_arr] = mc_polarization( double_map )
 %MC_POLARIZATION Evaluate the polarization at many angles
 %
-%   MC_POLARIZATION(double_map)
+%   [alpha, q] = MC_POLARIZATION(double_map)
 %   
-%   The angles values are defined in the PAR.POLARIZATION_ANGLES array
+%   The angles are defined in the PAR.POLARIZATION_ANGLES array
 
 angle = abs(PAR.POLARIZATION_ANGLES);
 Q_arr = [];
@@ -17,18 +17,20 @@ edges={linspace(-3.5,3.5,8),linspace(-3.5,3.5,8)};
 % Loop over different angles
 for theta = PAR.POLARIZATION_ANGLES
     atheta = abs(theta);
-    disp(['Theta = ',num2str(atheta)]);
     % Define "the polarization line"
     % The angular coefficient is ...
     m = tan( atheta*(pi/180.) );
-    disp(['m = ',num2str(m)]);
+    if PAR.LOG == 1
+        disp(['Theta = ',num2str(atheta)]);
+        disp(['m = ',num2str(m)]);
+    end
     x = linspace(-3.5,3.5,700)';
     y = (-1. / m) * x;  %-2.4143 * x; % 22.5 degree
     % Histogram the points of the line and then get the pixels with the line 
     % inside
     h = hist3([x, y], 'Edges', edges);
     matrix1 = h(1:end-1,1:end-1);
-    pixels = matrix1 > 10;
+    pixels = matrix1 > 1;
     % Evaluate the count belong the line
     scol = sum(double_map_small(pixels));
 
@@ -39,7 +41,7 @@ for theta = PAR.POLARIZATION_ANGLES
     % inside
     h = hist3([x, y], 'Edges', edges);  
     matrix2 = h(1:end-1,1:end-1);
-    pixels = matrix2 > 10;
+    pixels = matrix2 > 1;
     % Evaluate the count belong the line
     srow = sum(double_map_small(pixels));
     % Display the matrix (if theta < 0)
@@ -52,7 +54,9 @@ for theta = PAR.POLARIZATION_ANGLES
     % Estimate the polarization factor
     Q = (srow - scol ) / (srow + scol);
     Q_arr = [Q_arr Q];
-    disp(['Q = ',num2str(Q)]);
+    if PAR.LOG == 1
+        disp(['Q = ',num2str(Q)]);
+    end
 end
 
 end
