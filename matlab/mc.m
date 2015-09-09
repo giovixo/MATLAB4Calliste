43651019fdd2492231458351baa912d41217adb7
Giovanni De Cesare <decesare.giovanni@gmail.com>
2015-09-09 15:27:14 +0200

    better documentation

--- matlab/mc.m
+++ matlab/mc.m
@@ -6,16 +6,16 @@ disp('> Step 1...');
 ticID = tic; A = mc_read('01PolCrabCusp.dat'); toc(ticID);
 clear('ticID');
 disp('> The raw photons history A is ready.');
-%% Step 1: analysis example (see 'help mc_tools1' for more)
+%% Analysis of the level 1 (row) data (see 'help mc_tools1' for more)
 mc_tools1(A, 1);
 %% Step 2: Convert A into a 'photons history' into a pixelated detector
 disp('> Step 2...');
 ticID = tic; Apix = mc_pixelize(A); toc(ticID);
 clear('ticID');
 disp('> The pixelated photons history Apix is ready.');
-%% Step 2: analysis example (see 'help mc_tools2' for more)
+%% Analysis of the level 2 (pixelated detector) data (see 'help mc_tools2' for more)
 mc_tools2(Apix, 2);
-%% Level 3 analysis example (see 'help mc_polarization' for more
+%% Analysis of the level 2 (pixelated detector) data (see 'help mc_polarization' for more)
 load 'double_map.mat'
 [alpha, q]= mc_polarization(double_map);
 plot(alpha, q,'r*')
